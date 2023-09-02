9ad215100c32cd391c7dd98d8ef9635acdf1b8db
Souvik Bhattacharyya <souvikshanku@gmail.com>
2023-09-02 17:46:04 +0530

    multiplication works

--- polynomial.py
+++ polynomial.py
@@ -1,13 +1,17 @@
+from utils import lookup_log, lookup_antilog
+
+
 class GFPolynomial:
     def __init__(self, coeffs: list):
-        self.degree = len(coeffs)
+        # All coeffecients are assumed to be in [0, 255]
+        self.degree = len(coeffs) - 1
         self.coeffs = coeffs
         self.irrd_poly_coeff = [1, 0, 0, 0, 1, 1, 1, 1, 0]
 
     @staticmethod
     def _typecheck(p1, p2):
         if isinstance(p2, int):
-            p2 = GFPolynomial([0] * (p1.degree - 1) + [p2])
+            p2 = GFPolynomial([0] * p1.degree + [p2])
 
         elif isinstance(p2, GFPolynomial):
             if p1.degree > p2.degree:
@@ -19,6 +23,10 @@ class GFPolynomial:
             raise TypeError
 
         return p1, p2
+
+    @staticmethod
+    def gf_multiply(c1, c2):
+        return lookup_antilog[(lookup_log[c1] + lookup_log[c2]) % 255]
 
     def __repr__(self):
         poly = []
@@ -48,7 +56,23 @@ class GFPolynomial:
         return GFPolynomial(result)
 
     def __sub__(self, other):
-        return self._add_(other)
+        return self.__add__(other)
 
     def __mul__(self, other):
-        pass
+        result = [0] * (self.degree + other.degree + 1)
+
+        for i in range(self.degree + 1):
+            for j in range(other.degree + 1):
+                result[i+j] ^= self.gf_multiply(self.coeffs[i], other.coeffs[j])
+
+        return GFPolynomial(result)
+
+
+
+if __name__ == "__main__":
+    res = GFPolynomial([1, 1])
+
+    for i in range(1, 7):
+        res *= GFPolynomial([1, 2**i])
+
+    print(res)
